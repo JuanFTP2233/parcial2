0822d14646fd6c3b24525b36aabdd9ec91571f3f
JuanFTP2233 <juan.tarazona@uniminuto.edu.co>
2025-04-24 15:44:34 -0500

    2

--- src/main/java/com/example/Parcial2Ar/Repository/RepositoryJugador.java
+++ src/main/java/com/example/Parcial2Ar/Repository/RepositoryJugador.java
@@ -14,7 +14,7 @@ public interface RepositoryJugador extends JpaRepository<Jugador, Integer> {
     @Query(value = "SELECT * FROM jugador WHERE id_equipo = ?1", nativeQuery = true)
     List<Jugador> obtenerJugadoresPorEquipo(int idEquipo);
 
-    // Obtener los jugadores que han marcado más de X goles
+
     @Query(value = "SELECT * FROM jugador j JOIN estadistica_jugador e ON j.id_jugador = e.id_jugador WHERE e.goles > ?1", nativeQuery = true)
     List<Jugador> obtenerJugadoresPorGoles(int goles);
 }
